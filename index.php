ab4d6770693c9dfc258685b1365848794be96212
Sacha Mutschler <90270486+sachamutschler@users.noreply.github.com>
2022-02-23 16:24:01 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Sacha Mutschler</title>
+    <link rel="stylesheet" href="resources/css/style.css">
+</head>
+<body>
+    <div id="dossier_link">
+        <a href=""><img src="resources/img/dossier.png" alt="image de dossier" srcset="" id="dossier" class="dossier"> <!-- image par freepik --></a>
+        <a href=""><h1 id="titre" class="titre">Portfolio Sacha Mutschler</h1></a>
+    </div>
+    <div class="conteneur-menu">
+        <nav id="menu" class="menu">
+            <ul class="sous-menu">
+                <li class="ligne">Sacha Mutschler</li>
+                <li class="ligne">Compétences</li>
+                <li class="ligne">projets</li>
+                <li class="ligne">Contact</li>
+            </ul>
+        </nav>
+    </div>
+    <div id="navbar">
+        <img src="resources/img/logo-windows-noir.png" alt="logo windows noir" srcset="" class="logo_windows">
+    </div>
+</body>
+</html>
