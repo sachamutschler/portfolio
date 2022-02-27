9859e52386a9784eddd1d0ad1a4caf5e057a831b
Sachamutschler <sacha.mutschler@mediaschool.me>
2022-02-27 23:07:57 +0100

    Initialisation du projet

---
+++ dossier.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Sacha Mutschler</title>
+    <link rel="stylesheet" href="resources/css/style.css">
+</head>
+<body>
+    <div class="fichier" id="fichier">
+        
+    </div>
+    <div class="conteneur-menu">
+        <nav id="menu" class="menu">
+            <ul class="sous-menu">
+                <li class="ligne">Sacha Mutschler</li>
+                <li class="ligne">Compétences</li>
+                <li class="ligne">projets</li>
+                <li class="ligne">Contact</li>
+            </ul>
+        </nav>
+    </div>
+    <div id="navbar">
+        <img src="resources/img/logoxp.jpg" alt="logo windows noir" srcset="" class="logo_windows">
+    </div>
+</body>
+</html>
